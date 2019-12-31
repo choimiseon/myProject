83a3f04a77d6134380ff9e6904a34483807f6027
choimiseon <akrekffps1@nate.com>
2019-12-31 15:51:31 +0900

    json 테스트

---
+++ share/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	/*var charLength = $('.charLength').text();
+	console.log(charLength.length);
+	charLength.split(250).length;
+	console.log(charLength.split(250).length);
+	//var charLength = $('.charLength').split(0,250);
+	//console.log(charLength);
+	if(charLength.length >= 250){
+		
+		//return charLength.split(0,250)+"...";
+		//return 
+		//var text = charLength.slice(0,250)+"...";
+	    //return charLength.substr(0,250)+"...";
+		//$('.charLength').html(text);
+	}
+	*/
+});
